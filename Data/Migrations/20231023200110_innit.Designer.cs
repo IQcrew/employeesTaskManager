52f4c73cca178b5b459402a2065b20cacf42a799
TobiasMikula <94800323+IQcrew@users.noreply.github.com>
2023-10-23 22:05:19 +0200

    database

--- Data/Migrations/20231023200110_innit.Designer.cs
+++ Data/Migrations/20231023200110_innit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using employeesTaskManager.Data;
 
@@ -11,9 +12,10 @@ using employeesTaskManager.Data;
 namespace employeesTaskManager.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231023200110_innit")]
+    partial class innit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20231023200110_innit.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace employeesTaskManager.Data.Migrations
+{
+    public partial class innit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
